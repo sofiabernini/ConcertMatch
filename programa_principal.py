6c7f509362f82f1d94f1bd6cab7e8c8b2689264b
sofiabernini <sofiabelenbernini@gmail.com>
2026-06-01 15:20:59 -0300

    Create programa_principal.py
    Convierte DataSet en DataFrame

---
+++ programa_principal.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 15:18:34 2026
+
+@author: sofia
+"""
+
+#PROGRAMA PRINCIPAL:
+    
+#Primero transformamos el DataSet en un DataFrame de Python
+
+import pandas as pd
+
+df = pd.read_csv("conciertos_buenos_aires.csv")
+

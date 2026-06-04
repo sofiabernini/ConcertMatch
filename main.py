6a4a44cdd8ea3361ec077a72772e80d98ed55994
sofiabernini <sofiabelenbernini@gmail.com>
2026-06-04 14:31:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ Created on Mon Jun  1 15:18:34 2026
 
 import pandas as pd
 
-df = pd.read_csv("conciertos_buenos_aires.csv")
+# Leer el dataset que está dentro de la carpeta 'datos'
+# encoding='utf-8' --> hace que los nombres con acentos o eñes no generen un error de decodificación
+df = pd.read_csv('datos/concertmatch_dataset_prueba.csv', encoding='utf-8')
+
+# Ver las primeras 5 filas para comprobar que se cargó correctamente
+print(df.head()) #esto se borra después, era para probar nomás
 

973e77a7cf5bebaf98ca3bc15a3e4fa246240b30
sofiabernini <sofiabelenbernini@gmail.com>
2026-06-05 12:43:54 -0300

    Update main.py
    - Se agregó la validación de la carga del Dataframe.
    - Se pregunta por el acceso para movilidad reducida y se actualiza el dataframe según corresponde.
    - Se pregunta al final si se desea volver a ejecutar el programa o finalizar.

--- main.py
+++ main.py
@@ -10,11 +10,62 @@ Created on Mon Jun  1 15:18:34 2026
 #Primero transformamos el DataSet en un DataFrame de Python
 
 import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 # Leer el dataset que está dentro de la carpeta 'datos'
 # encoding='utf-8' --> hace que los nombres con acentos o eñes no generen un error de decodificación
-df = pd.read_csv('datos/concertmatch_dataset_prueba.csv', encoding='utf-8')
+try:
+    df = pd.read_csv('datos/concertmatch_dataset_prueba.csv', encoding='utf-8')
+except FileNotFoundError:
+    print("Error: El DataFrame no fue encontrado.")
+    
+except Exception as e:
+    print(f"Ocurrió un error inesperado al cargar la base de datos: {e}")
 
-# Ver las primeras 5 filas para comprobar que se cargó correctamente
-print(df.head()) #esto se borra después, era para probar nomás
+#Mensaje de Bienvenida:
+print("\n" + "="*50)
+print("🎸 BIENVENIDO A CONCERTMATCH V2 🎸")
+print("="*50)
+print("A continuación te haremos una serie de preguntas para determinar tus preferencias y así recomendarte 5 eventos disponibles que coinciden con tus gustos.")
 
+#Pregunta si necesita acceso para personas con movilidad reducida:
+while True:
+    movilidad = input("¿Necesitas acceso para personas con movilidad reducida? (si/no): ").strip().lower()
+    if movilidad == "si":
+        #df_requiere_movilidad = filtrar_dataset_bool(condicion == True, columna == movilidad )
+        if df_requiere_movildad != #vacío:
+            break
+        else:
+            print("Lo sentimos, actualmente no hay eventos disponibles con acceso para movilidad reducida.")
+            while True:
+                continuar = input("¿Deseas continuar buscando eventos sin este filtro? (si/no): ").strip().lower()
+                if continuar == "si":
+                    break
+                elif continuar == "no":
+                    print("¡Gracias por usar ConcertMatch!")
+                    break
+                else:
+                    print("Opción no válida. Escriba 'si' o 'no'.")
+    
+    elif movilidad == "no":
+        break
+        
+    else:
+        print("Opción no válida. Escriba 'si' o 'no'.")
+        
+        
+        
+#Esto va al final de todo el programa, después de mostrar los resultados/graáficos:
+#Pregunta si desea volver a ejecutar el programa:
+while True:
+    reintentar = input("¿Deseas realizar una nueva búsqueda? (si/no): ").strip().lower()
+    if reintentar == "si":
+        #¿Llamar a la función "ejecutar_programa"?
+        
+    elif reintentar == "no":
+        print("¡Gracias por usar ConcertMatch! Esperamos que disfrutes del evento. 🎶")
+        break
+    
+    else:
+        print("Opción no válida. Escriba 'si' o 'no'.")

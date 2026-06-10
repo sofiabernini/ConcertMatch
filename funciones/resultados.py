afdf25638a7683e0245314e1869178aab02b1d6e
victoriamochnacs <victoriamochnacs@gmail.com>
2026-06-10 15:01:13 -0300

    obtener_mejores en resultados.py

--- funciones/resultados.py
+++ funciones/resultados.py
@@ -5,6 +5,30 @@ Created on Tue Jun  9 16:34:42 2026
 @author: sofia
 """
 
+#en este archivo se obtienen los datos para crear los gráficos
+#   - obtener_mejores (df, columna_porcentaje = 'porcentaje_coincidencia', top_n=5)
+#   - mostrar_info_resultados (df_mejores)
+#   - grafico_resultado()
+
+
+
+# FUNCIÓN PARA OBTENER LOS 5 MEJORES RESULTADOS
+
+def obtener_mejores(df, columna_porcentaje='porcentaje_coincidencia', top_n=5):
+    """
+    Ordena el DataFrame según el porcentaje de coincidencia de mayor a menor
+    y devuelve los 'top_n' mejores resultados. (en este caso, el top 5)
+    """
+    if df.empty:
+        return df
+        
+    # Ordenamos de mayor a menor coincidencia
+    df_ordenado = df.sort_values(by=columna_porcentaje, ascending=False)
+    
+    # Devolvemos solo las primeras 'top_n' filas (por defecto 5)
+    return df_ordenado.head(top_n)
+
+
 # 1. FUNCIÓN PARA MOSTRAR LA INFORMACIÓN EN CONSOLA
 
 def mostrar_info_resultados(df_mejores):
